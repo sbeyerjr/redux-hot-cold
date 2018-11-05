7529180036fb08a257d62c3a5900c4723cf18940
Steven Beyer <sbeyerjr@gmail.com>
2018-11-04 18:28:24 -0700

    reducer and action testing added

---
+++ src/actions.test.js
@@ -0,0 +1,25 @@
+import {RESTART_GAME, restartGame, MAKE_GUESS, makeGuess, GENERATE_AURAL_UPDATE, generateAuralUpdate} from './actions';
+
+describe('generateAuralUpdate', () => {
+    it('Should return the action', () => {
+        const action = generateAuralUpdate();
+        expect(action.type).toEqual(GENERATE_AURAL_UPDATE);
+       
+    });
+});
+describe('restartGame', () => {
+    it('Should return the action', () => {
+        const correctAnswer = 12;
+        const action = restartGame(correctAnswer);
+        expect(action.type).toEqual(RESTART_GAME);
+        expect(action.correctAnswer).toEqual(correctAnswer);
+    });
+});
+describe('makeGuess', () => {
+    it('Should return the action', () => {
+        const guess = 12;
+        const action = makeGuess(guess);
+        expect(action.type).toEqual(MAKE_GUESS);
+        expect(action.guess).toEqual(guess);
+    });
+});
